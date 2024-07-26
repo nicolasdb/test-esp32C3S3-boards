0683e872f39269281a7088a8d13890aa4a31dfd9
Nicolas de Barquin <nicolas@openfab.be>
2024-07-26 10:26:20 +0200

    stampS5 simple test

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,74 @@
+/*
+*******************************************************************************
+* Copyright (c) 2022 by M5Stack
+*                  Equipped with StampS3 sample source code
+*                          配套  StampS3 示例源代码
+* Visit for more information: https://docs.m5stack.com/en/core/stamps3
+* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/core/stamps3
+*
+* Describe: LED Show example.  LED展示示例
+* Date: 2023/1/11
+*******************************************************************************
+Press button to change LED status
+*/
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define LED_PIN     9
-#define BRIGHTNESS  25
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define MAX_DIMENSION 25  // Maximum dimension we'll ever use
-#define RAINDROP_FREQUENCY 100  // Higher values (up to 255) will create more frequent raindrops
+#define PIN_BUTTON 0
+#define PIN_LED    21
+#define NUM_LEDS   1
 
-// Rain parameters
-#define LIGHT_RAIN 10
-#define MEDIUM_RAIN 25
-#define HEAVY_RAIN 50
+CRGB leds[NUM_LEDS];
+uint8_t led_ih             = 0;
+uint8_t led_status         = 0;
+String led_status_string[] = {"Rainbow", "Red", "Green", "Blue"};
 
-uint8_t currentRainIntensity = MEDIUM_RAIN;
-uint8_t dropSpeed = 1;  // Pixels per update
-uint8_t updateInterval = 50;  // Milliseconds between updates
-
-
-enum MatrixOrientation {
-  TOP_LEFT_HORIZONTAL,
-  TOP_LEFT_VERTICAL,
-  BOTTOM_LEFT_HORIZONTAL,
-  BOTTOM_LEFT_VERTICAL
-};
-
-struct MatrixConfig {
-  uint8_t width;
-  uint8_t height;
-  MatrixOrientation orientation;
-  bool zigzag;
-};
-
-MatrixConfig matrix = {8, 8, TOP_LEFT_VERTICAL, false};
-// with {width, height, orientation mode, zigzag mode} as define just before
-
-#define NUM_LEDS (matrix.width * matrix.height)
-CRGB leds[MAX_DIMENSION * MAX_DIMENSION];  // Allocate for maximum possible size
-uint8_t raindrops[MAX_DIMENSION] = {0};  // Allocate for maximum possible width
-
-
-
-uint16_t XY(uint8_t x, uint8_t y) {
-    uint16_t i;
-    switch(matrix.orientation) {
-        case TOP_LEFT_HORIZONTAL:
-            if (matrix.zigzag && y % 2 == 1) {
-            x = (matrix.width - 1) - x;
-            }
-            i = (y * matrix.width) + x;
-            break;
-        case TOP_LEFT_VERTICAL:
-            if (matrix.zigzag && x % 2 == 1) {
-            y = (matrix.height - 1) - y;
-            }
-            i = (x * matrix.height) + y;
-            break;
-        case BOTTOM_LEFT_HORIZONTAL:
-            y = (matrix.height - 1) - y;
-            if (matrix.zigzag && y % 2 == 1) {
-            x = (matrix.width - 1) - x;
-            }
-            i = (y * matrix.width) + x;
-            break;
-        case BOTTOM_LEFT_VERTICAL:
-            x = (matrix.width - 1) - x;
-            if (matrix.zigzag && x % 2 == 1) {
-            y = (matrix.height - 1) - y;
-            }
-            i = (x * matrix.height) + y;
-            break;
-    }
-    return i;
-}
-
+/* After StampS3 is started or reset
+   the program in the setUp () function will be run, and this part will only be
+   run once.
+*/
 void setup() {
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(BRIGHTNESS);
+    USBSerial.begin(115200);
+    USBSerial.println("StampS3 demo!");
 
-// Initialize raindrops array (not strictly necessary with static initialization, but good practice)
-  for (int i = 0; i < MAX_DIMENSION; i++) {
-    raindrops[i] = 0;
-  }
+    pinMode(PIN_BUTTON, INPUT);
+
+    FastLED.addLeds<WS2812, PIN_LED, GRB>(leds, NUM_LEDS);
 }
 
-// Function to change rain intensity. determines how often new raindrops are created.
-void setRainIntensity(uint8_t intensity) {
-  currentRainIntensity = intensity;
-}
-
-// Function to change drop speed. controls how many pixels a raindrop falls in each update.
-void setDropSpeed(uint8_t speed) {
-  dropSpeed = speed;
-}
-
+/* After the program in setup() runs, it runs the program in loop()
+  The loop() function is an infinite loop in which the program runs repeatedly
+*/
 void loop() {
-  static unsigned long lastUpdate = 0;
-  unsigned long currentTime = millis();
-
-  if (currentTime - lastUpdate >= updateInterval) {
-    lastUpdate = currentTime;
-
-    FastLED.clear();
-
-    // Update and draw raindrops
-    for (int x = 0; x < matrix.width; x++) {
-      if (raindrops[x] == 0 && random8() < currentRainIntensity) {
-        raindrops[x] = matrix.height;  // Start from just above the top row
-      }
-
-      if (raindrops[x] > 0) {
-        raindrops[x] = (raindrops[x] > dropSpeed) ? raindrops[x] - dropSpeed : 0;
-        if (raindrops[x] < matrix.height) {  // Only draw if within the matrix
-          leds[XY(x, raindrops[x])] = CRGB::Blue;
-        }
-      }
+    switch (led_status) {
+        case 0:
+            leds[0] = CHSV(led_ih, 255, 255);
+            break;
+        case 1:
+            leds[0] = CRGB::Red;
+            break;
+        case 2:
+            leds[0] = CRGB::Green;
+            break;
+        case 3:
+            leds[0] = CRGB::Blue;
+            break;
+        default:
+            break;
     }
-
     FastLED.show();
-  }
+    led_ih++;
+    delay(15);
+
+    if (!digitalRead(PIN_BUTTON)) {
+        delay(5);
+        if (!digitalRead(PIN_BUTTON)) {
+            led_status++;
+            if (led_status > 3) led_status = 0;
+            while (!digitalRead(PIN_BUTTON))
+                ;
+            USBSerial.print("LED status updated: ");
+            USBSerial.println(led_status_string[led_status]);
+        }
+    }
 }
