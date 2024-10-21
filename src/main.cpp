602d3ae526906fbd7691f85d566ea832a760678b
Nicolas de Barquin <nicolas.de.barquin@gmail.com>
2024-10-21 16:23:19 +0200

    now LED, button, relay and webserver are working

--- src/main.cpp
+++ src/main.cpp
@@ -4,121 +4,76 @@
 #include <WiFi.h>
 #include <SPIFFS.h>
 #include "WebInterface.h"
-#include "RFIDManager.h"
 #include "credentials.h"
 
-// Pin definitions for different boards
-#if defined(CONFIG_IDF_TARGET_ESP32C3)
-    #define PIN_BUTTON 9
-    #define PIN_LED 2
-    #define PIN_RELAY 3
-    #define RST_PIN 10
-    #define SS_PIN  7
-#elif defined(CONFIG_IDF_TARGET_ESP32S3)
-    #define PIN_BUTTON 7
-    #define PIN_LED 14
-    #define PIN_RELAY 15
-    #define RST_PIN 10
-    #define SS_PIN  11
-#elif defined(ARDUINO_M5STACK_STAMPS3)
-    #define PIN_BUTTON 0
-    #define PIN_LED 21
-    #define PIN_RELAY 22
-    #define RST_PIN 10
-    #define SS_PIN  11
-#else
-    #error "Unsupported board selected"
-#endif
-
+#define PIN_BUTTON 9
+#define PIN_RELAY 3
+#define PIN_NEOPIXEL 2
 #define NUM_LEDS 1
+
 #define DOOR_OPEN_TIME 5000 // 5 seconds in milliseconds
 
 CRGB leds[NUM_LEDS];
+
 bool doorOpen = false;
 unsigned long doorOpenTime = 0;
 
 WebInterface webInterface;
-RFIDManager rfidManager(SS_PIN, RST_PIN);
 
-void setupWiFi();
-void checkWiFiStatus();
 void setDoorState(bool open);
 void checkButton();
-void checkDoorTimer();
-void controlDoorHardware(bool open);
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("ESP32 Door Control System - Setup started");
+    Serial.println("ESP32 Door Control System");
 
     pinMode(PIN_BUTTON, INPUT_PULLUP);
     pinMode(PIN_RELAY, OUTPUT);
-    FastLED.addLeds<WS2812, PIN_LED, GRB>(leds, NUM_LEDS);
-    
+    digitalWrite(PIN_RELAY, LOW);
+
+    FastLED.addLeds<NEOPIXEL, PIN_NEOPIXEL>(leds, NUM_LEDS);
+    FastLED.setBrightness(50);
+
+    // Initialize SPIFFS
     if (!SPIFFS.begin(true)) {
         Serial.println("An error has occurred while mounting SPIFFS");
+        return;
     }
-    
-    setupWiFi();
+
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Initialize WebInterface
     webInterface.setup();
-    rfidManager.setup();
-    
-    Serial.println("Setup completed");
+
+    Serial.printf("PIN_BUTTON: %d, PIN_RELAY: %d, PIN_NEOPIXEL: %d\n", PIN_BUTTON, PIN_RELAY, PIN_NEOPIXEL);
 }
 
 void loop() {
-    checkWiFiStatus();
     checkButton();
-    checkDoorTimer();
-    
-    if (rfidManager.checkCard()) {
-        setDoorState(true);
-    }
-    
     webInterface.handleClient();
-    
+
+    if (doorOpen && (millis() - doorOpenTime >= DOOR_OPEN_TIME)) {
+        setDoorState(false);
+    }
+
+    FastLED.show();
     delay(10);
-}
-
-void setupWiFi() {
-    Serial.println("Connecting to WiFi...");
-    WiFi.begin(ssid, password);
-    
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20) {
-        delay(500);
-        Serial.print(".");
-        attempts++;
-    }
-    
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nWiFi connected");
-        Serial.println("IP address: " + WiFi.localIP().toString());
-    } else {
-        Serial.println("\nFailed to connect to WiFi");
-    }
-}
-
-void checkWiFiStatus() {
-    static unsigned long lastCheck = 0;
-    if (millis() - lastCheck > 10000) {  // Check every 10 seconds
-        lastCheck = millis();
-        if (WiFi.status() != WL_CONNECTED) {
-            Serial.println("WiFi disconnected. Attempting to reconnect...");
-            setupWiFi();
-        }
-    }
 }
 
 void setDoorState(bool open) {
     doorOpen = open;
-    controlDoorHardware(doorOpen);
-    if (doorOpen) {
-        doorOpenTime = millis();
-        Serial.println("Door opened");
-    } else {
-        Serial.println("Door closed");
-    }
+    doorOpenTime = millis();
+    digitalWrite(PIN_RELAY, open ? HIGH : LOW);
+    leds[0] = open ? CRGB::Green : CRGB::Red;
+    Serial.printf("Door %s\n", open ? "opened" : "closed");
     webInterface.sendUpdate(doorOpen);
 }
 
@@ -134,19 +89,4 @@ void checkButton() {
         }
     }
     lastButtonState = buttonState;
-}
-
-void checkDoorTimer() {
-    if (doorOpen && (millis() - doorOpenTime >= DOOR_OPEN_TIME)) {
-        setDoorState(false);
-    }
-}
-
-void controlDoorHardware(bool open) {
-    // Control LED
-    leds[0] = open ? CRGB::White : CRGB::Black;
-    FastLED.show();
-
-    // Control Relay
-    digitalWrite(PIN_RELAY, open ? HIGH : LOW);
 }
